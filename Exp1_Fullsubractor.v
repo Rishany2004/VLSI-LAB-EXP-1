feb9100aa10b207e8549b88a09bf7d212a921c2d
Rishany2004 <159290227+Rishany2004@users.noreply.github.com>
2024-05-06 22:45:19 +0530

    Update Exp1_Fullsubractor.v

--- Exp1_Fullsubractor.v
+++ Exp1_Fullsubractor.v
@@ -1,7 +1,12 @@
-module fs(a,b,bin,d,bout);
-input a,b,bin;
-output d,bout;
-
-
-
+module fsubt(a,b,c,diff,borr);
+input a,b,c;
+output diff,borr;
+wire w1,w2,w3,w4,w5;
+xor g1(w2,a,b);
+not g2(w1,a); 
+xor g3(diff,w2,c);
+and g4(w4,w1,b);
+not g5(w3,w2);
+and g6(w5,w3,c);
+or g7(borr,w5,w4);
 endmodule
