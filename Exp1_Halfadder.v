7e9ab0bb325ff0c8c60e516c90cb4dd2e75cbac0
Rishany2004 <159290227+Rishany2004@users.noreply.github.com>
2024-05-06 22:46:38 +0530

    Update Exp1_Halfadder.v

--- Exp1_Halfadder.v
+++ Exp1_Halfadder.v
@@ -1,5 +1,11 @@
-module ha(a,b,sum,carry);
-input a,b;
-output sum,carry;
-
+module fadder(a,b,c,sum,carry);
+input a,b,c;
+output sum ,carry;
+wire w1,w2,w3;
+xor g1(w1,a,b);
+and g2(w3,a,b);
+xor g3(sum,w1,c);
+and g4(w2,w1,c);
+or g5(carry,w2,w3);
 endmodule
+
